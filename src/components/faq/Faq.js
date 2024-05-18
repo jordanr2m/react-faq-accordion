e4f7d6ae8a82782d4436de7545e39c4c3db9cfd8
Jordan J <jordanr2m@gmail.com>
2024-05-18 11:11:30 -0700

    Finsihed adding styles. Added logic and completed project.

--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -3,26 +3,32 @@ import { MdOutlineLibraryBooks } from 'react-icons/md';
 /* You must import react icons into your project in order to use it */
 import Question from './Question';
 import {questions} from "./data";
-// Import questions from data.js
+// Import questions from data.js in order to access props
+import "./Faq.css";
+// import Faq css styles
 
 /* We can style the react icon inline as shown below */
 const Faq = () => {
   return (
     <section className='faq-sec'>
-      <div className='contianer faq'>
-        <div className="title --center-all">
-            <MdOutlineLibraryBooks size={30} color="orangered"/>
-            <h2 className='--mb2'>FAQs</h2>
-            <p className='--text-small'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+        <div className='container faq'>
+            <div className="title --center-all">
+                <MdOutlineLibraryBooks size={30} color="orangered"/>
+                <h2 className='--mb2'>FAQs</h2>
+                <p className='--text-small'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            </div>
+            <div className='questions'>
+                {/* Map over questions and return a Question component for each. Uses implied return statement */}
+                {questions.map((question) => (
+                    // Pass 2 props in (question & answer) must also pass in key whenever using map in react
+                    <Question 
+                        key={question.id} 
+                        title={question.title} 
+                        answer={question.answer}
+                    />
+                ))}
+            </div>
         </div>
-        <div className='questions'>
-            {/* Map over questions and return a Question component for each. Uses implied return statement */}
-            {questions.map((question) => (
-                // Pass 2 props in (question & answer)
-                <Question title={question.title} answer={question.answer}/>
-            ))}
-        </div>
-      </div>
     </section>
   )
 }
--- src/components/faq/Question.js
+++ src/components/faq/Question.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react'
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import "./Question.css";
 
-/* We need to use props to access the question info */
+/* We need to use props to access each question's info. Passed down from FAQ component, which calls the Question component from it */
 const Question = ({title, answer}) => {
+    // set showAnswer to false by default
     const [showAnswer, setShowAnswer] = useState(false);
 
     const handleClick = () => {
@@ -13,20 +14,21 @@ const Question = ({title, answer}) => {
 
   return (
     <div className='container question --card'>
-      <div className='question-title --flex-between'>
-        <h4 className='--text-p --fw-bold'>{title}</h4>
-        <button className='question-icon' onClick={handleClick}>
-            {/* If showAnswer is true, a plus icon is shown. Otherwise, a minus icon is shown */}
-            {showAnswer ? (
-                <AiOutlineMinus color="red" />
-            ) : (
-                <AiOutlinePlus color="#1f93ff" />
-            )}
-        </button>
-      </div>
-      <div className='question-answer'>
+        <div className='question-title --flex-between'>
+            <h4 className='--text-p --fw-bold'>{title}</h4>
+            <button className='question-icon' onClick={handleClick}>
+                {/* If showAnswer is true, a minus icon is shown. Otherwise, a plus icon is shown */}
+                {showAnswer ? (
+                    <AiOutlineMinus color="red" />
+                ) : (
+                    <AiOutlinePlus color="#1f93ff" />
+                )}
+            </button>
+        </div>
+        <div className='question-answer'>
+            {/* Saying if showAnswer is true, then show p with answer text */}
             {showAnswer && <p className='--text-sm --py'>{answer}</p>}
-      </div>
+        </div>
     </div>
   )
 }
